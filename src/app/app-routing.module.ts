eab38be29380bc3fa77ae2ebd3f5419a86f1d8da
Maytton <maytton0000@gmail.com>
2024-03-21 22:11:43 -0300

    Criei o service e adicionei a biblioteca Http

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+{ path: '', pathMatch: 'full', redirectTo: 'courses'},
+{ path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) // caminho do modulo filho
+}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
